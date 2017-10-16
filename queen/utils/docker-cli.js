f8b65fffdb81ebf7bae014511a0bd6ad1a769db6
Vladimir Yartsev <vovayartsev@gmail.com>
2017-10-16 10:34:55 +0400

    Fix secret handling via STDIN

--- queen/utils/docker-cli.js
+++ queen/utils/docker-cli.js
@@ -1,6 +1,5 @@
 const { writeFileSync } = require("fs")
-const tmp = require('tmp')
-const {promisify} = require('util')
+const { promisify } = require('util')
 const { execFile } = require('child_process')
 const execDocker = args => promisify(execFile)("docker", args)
 
@@ -8,20 +7,21 @@ module.exports = {
   dockerSwarmInit: () => execDocker(["swarm", "init", "--advertise-addr", "eth0"]),
   dockerSwarmJoin: (joinToken, joinAddr) => execDocker(["swarm", "join", "--token", joinToken, joinAddr]),
   dockerCmd: ({command, args, stdin}) => {
-    // handling https://github.com/cloudcastle/swarm-queen/issues/6
-    if (typeof(stdin) !== "null" && typeof(stdin) !== "undefined") {
-      substituteStdinWithTempfile(args, stdin)
-    }
-    return execDocker([command, ...args])
-  }
-}
+    return new Promise((resolve, reject) => {
+      const child = execFile("docker", [command, ...args], (error, stdout, stderr) => {
+        if (error) {
+          reject (error)
+        } else {
+          resolve({stdout, stderr})
+        }
+      })
 
-function substituteStdinWithTempfile(args, stdin) {
-  args.forEach((arg, index) => {
-    if (arg === "-") {
-      const tmpFile = tmp.fileSync()
-      writeFileSync(tmpFile.name, stdin)
-      args[index] = tmpFile.name
-    }
-  })
+      // handling https://github.com/cloudcastle/swarm-queen/issues/6
+      if (typeof(stdin) !== "null" && typeof(stdin) !== "undefined") {
+        child.stdin.write(stdin)
+      }
+
+      child.stdin.end()
+    })
+  }
 }
